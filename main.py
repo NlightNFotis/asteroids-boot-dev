1a2c4f67f8e4cf477cddb30731f233be873fad74
Fotis Koutoulakis <fotis.koutoulakis@gmail.com>
2025-02-20 23:27:46 +0000

    feat: add delta time to decouple execution from in-game time

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 
 from constants import *
+import utils
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0  # delta time
 
     # Main game loop
     while True:
@@ -14,8 +18,8 @@ def main():
 
         screen.fill('black')
         pygame.display.flip()
+        dt = utils.to_seconds(clock.tick(60))
         
-
 
 if __name__ == '__main__':
     main()
---
+++ utils.py
@@ -0,0 +1,2 @@
+def to_seconds(ms):
+    return ms / 1000
